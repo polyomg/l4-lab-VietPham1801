1ee8499701ccaa53998dce71178e9d56a18784a7
Vietpham <vietpqts00881@fpt.edu.vn>
2025-10-04 14:17:05 +0700

    TS00881_PhamQuocViet

---
+++ Lab3/src/main/java/com/poly/edu/lab3/controller/Bai2Controller.java
@@ -0,0 +1,26 @@
+package com.poly.edu.lab3.controller;
+
+import com.poly.edu.lab3.model.Staff;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class Bai2Controller {
+
+    @RequestMapping("/staff/list")
+    public String list(Model model) {
+        List<Staff> list = List.of(
+                Staff.builder().id("user1@gmail.com").fullname("nguyễn văn user1").photo("user.png").salary(12345.6789).level(0).build(),
+                Staff.builder().id("user2@gmail.com").fullname("nguyễn văn user2").photo("user.png").salary(12345.6789).level(1).build(),
+                Staff.builder().id("user3@gmail.com").fullname("nguyễn văn user3").photo("user.png").salary(12345.6789).level(2).build(),
+                Staff.builder().id("user4@gmail.com").fullname("nguyễn văn user4").photo("user.png").salary(12345.6789).level(2).build(),
+                Staff.builder().id("user5@gmail.com").fullname("nguyễn văn user5").photo("user.png").salary(12345.6789).level(1).build(),
+                Staff.builder().id("user6@gmail.com").fullname("nguyễn văn user6").photo("user.png").salary(12345.6789).level(0).build()
+        );
+        model.addAttribute("list", list);
+        return "staff-list";
+    }
+}
